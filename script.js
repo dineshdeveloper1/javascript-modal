8b338f3d7b696d59d9fbdc99b485099f844b6be9
Dinesh Kumar <dinesh78088@gmail.com>
2025-02-13 19:56:09 +0530

    code update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let modal = document.querySelector('.modal-container');
+
+function openModalBox() {
+    modal.style.display = 'flex';
+}
+
+function closeModalBox() {
+    modal.style.display = 'none';
+}
